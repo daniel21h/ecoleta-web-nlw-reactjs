db7d62fc4eb5e71515a7ce4c5f0a7832d138a0e2
daniel21h <hesseldaniel21@gmail.com>
2021-01-22 16:51:32 -0300

    feat: prepared to deploy

---
+++ build/precache-manifest.c449c09b3b622f7482e54da83b87c53f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca11358fe106785a19cbb0eb88693a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35036a116ef0b9a7e28d",
+    "url": "/static/css/main.ffd9d439.chunk.css"
+  },
+  {
+    "revision": "9155943fa405138afd24",
+    "url": "/static/js/2.b2d80f81.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.b2d80f81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35036a116ef0b9a7e28d",
+    "url": "/static/js/main.a19cd753.chunk.js"
+  },
+  {
+    "revision": "d0a598c2c4f910599d51",
+    "url": "/static/js/runtime-main.e7310a31.js"
+  },
+  {
+    "revision": "abd2ba69bb6bf8469130b703115dd2a2",
+    "url": "/static/media/home-background.abd2ba69.svg"
+  },
+  {
+    "revision": "26434f1f8d4600c2289a91f27afe85a8",
+    "url": "/static/media/logo.26434f1f.svg"
+  }
+]);
